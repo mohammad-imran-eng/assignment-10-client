adbdced98f0435c285dfb23d3083a0146ba66547
Mohammad Imran <immu.engineer@gmail.com>
2025-01-31 10:22:33 +0600

    change color home layout design

--- src/Layouts/HomeLayout/HomeLayout.jsx
+++ src/Layouts/HomeLayout/HomeLayout.jsx
@@ -11,11 +11,11 @@ const HomeLayout = () => {
                 <div className="h-[400px] flex items-center justify-center bg-gradient-to-r from-gray-800 to-gray-900">
             <div className="text-center space-y-8">
                 <Slide direction="left" cascade triggerOnce>
-                <div className="feature flex items-center justify-center space-x-4 bg-white bg-opacity-20 backdrop-blur-lg rounded-lg shadow-lg p-6">
+                <div className="text-white bg-gray-600 feature flex items-center justify-center space-x-4  bg-opacity-20 backdrop-blur-lg rounded-lg shadow-lg p-6">
                     <FaLightbulb className="text-4xl text-yellow-400" />
                     <span className="text-2xl font-semibold ">💡 Gorgeous</span>
                 </div>
-                <div className="feature flex items-center justify-center space-x-4 bg-white bg-opacity-20 backdrop-blur-lg rounded-lg shadow-lg p-6">
+                <div className="text-white feature flex items-center justify-center space-x-4 bg-gray-600 bg-opacity-20 backdrop-blur-lg rounded-lg shadow-lg p-6">
                     <FaPalette className="text-4xl text-pink-400" />
                     <span className="text-2xl font-semibold ">🎨 Customization</span>
                 </div>
